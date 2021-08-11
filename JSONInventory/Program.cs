e95cb0d07f73cc3b40c59d9b4a29c3c8f706f6fc
vk0808 <vk4765712@gmail.com>
2021-08-11 18:11:04 +0530

    feat: initial commit

---
+++ JSONInventory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSONInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to JSON Inventory Data Management System\n");
+        }
+    }
+}
